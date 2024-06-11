235db10a527c8d6ec78cc67b21139f9d64345bdc
anton <antonkuzmn1@gmail.com>
2024-06-11 11:43:03 +0700

    generated the missing files for the build

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { HttpClientModule } from '@angular/common/http';
@@ -6,6 +6,7 @@ import { provideEnvironmentNgxMask } from 'ngx-mask';
 import { routes } from './app.routes';
 import { BackendConfig } from './common/backend.config';
 import { ModalComponent } from './common/modal.component';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,5 +15,9 @@ export const appConfig: ApplicationConfig = {
     provideEnvironmentNgxMask(),
     BackendConfig,
     ModalComponent,
-  ],
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
 };
