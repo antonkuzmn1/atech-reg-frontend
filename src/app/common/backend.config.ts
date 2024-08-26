ac11334950ff4f97f8c999890b8a267dfaa9cbcc
anton <antonkuzmn1@gmail.com>
2024-08-26 19:47:14 +0700

    fixed local server path

--- src/app/common/backend.config.ts
+++ src/app/common/backend.config.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 export class BackendConfig {
   public auto(path: string = ''): string {
     if (window.location.origin === 'http://localhost:4200')
-      return 'http://localhost:8080/api/' + path;
+      return 'http://localhost:8080/' + path;
     else return window.location.origin + '/api/' + path;
   }
 
